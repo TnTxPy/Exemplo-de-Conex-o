4d32c73f6d4cfa31b732f346c70807a8d7d8df16
Lucas <Lucas@Lucas-PC>
2018-08-04 13:30:02 -0300

    v1.0.3 - MENU PRINCIPAL

--- Exemplo de Conexão/src/main/Principal.java
+++ Exemplo de Conexão/src/main/Principal.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.Scanner;
+import static main.Funcao.print;
 import objetos.Usuario;
 
 public class Principal extends Funcao {
@@ -16,16 +17,77 @@ public class Principal extends Funcao {
         */
         
         Scanner tc = new Scanner(System.in);
-        Funcao f = new Funcao();
+        Scanner tc1 = new Scanner(System.in);
         boolean continuar = true;
         int op;
         while(continuar){
-            
-            f.print("CONEXAO SQL");
-            f.print("MENU PRINCIPAL");
-            f.print("1. Verificar Existencia");
-            f.print("2. Criar Usuário       ");
+            print("Bem vindo ao sistema de Conexão sql");
+            print("    MENU PRINCIPAL    ");
+            print("1. Cadastrar Usuario  ");
+            print("2. Verificar Usuario  ");
+            print("3. Remover Usuario    ");
+            print("");
             op = tc.nextInt();
+            if(op == 1){
+                
+                boolean continuarOp1 = true;
+                String nome = "",login = "",senha = "";
+                
+                while(continuarOp1){
+                    print("    CADASTRO DE USUARIO    ");
+                    print("    Insira o Nome:         ");
+                    try {
+                        nome = tc1.nextLine();
+                        continuarOp1 = false;
+                    } catch (Exception e) {
+                        print("[ERRO] INSIRA UM NOME VALIDO",true);
+                    }
+                }
+                continuarOp1 = true;
+                while(continuarOp1){
+                    print("     CADASTRO DE LOGIN     ");
+                    print("    Insira o Login:         ");
+                    try {
+                        login = tc1.nextLine();
+                        continuarOp1 = false;
+                    } catch (Exception e) {
+                        print("[ERRO] INSIRA UM LOGIN VALIDO",true);
+                    }
+                }
+                continuarOp1 = true;
+                while(continuarOp1){
+                    print("     CADASTRO DE SENHA      ");
+                    print("    Insira a Senha:         ");
+                    try {
+                        senha = tc1.nextLine();
+                        continuarOp1 = false;
+                    } catch (Exception e) {
+                        print("[ERRO] INSIRA UMA SENHA VALIDA",true);
+                    }
+                }
+                
+                
+                Usuario newU = new Usuario();
+                newU.setLogin(login);
+                newU.setNome(nome);
+                newU.setSenha(senha);
+                newU.criar();
+                if(newU.existe()){
+                    print("CONTA CRIADA COM SUCESSO");
+                }else{
+                    print("[ERRO] FALHA NA CRIAÇÃO DA CONTA",true);
+                }
+                
+                
+                
+                
+            }else if(op == 2){
+                
+            }else if(op == 3){
+                
+            }else{
+                
+            }        
         }
         
         
