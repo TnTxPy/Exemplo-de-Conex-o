2d7b6b9409801662bf529cc1b4f8a63c448eddce
TnTxPy <lucas1999mendes@gmail.com>
2018-08-04 13:30:41 -0300

    v1.0.3 - MENU PRINCIPAL

--- Exemplo de Conexão/src/main/Principal.java
+++ Exemplo de Conexão/src/main/Principal.java
@@ -7,7 +7,6 @@ import objetos.Usuario;
 public class Principal extends Funcao {
 
     public static void main(String[] args) {
-        
         /*
         Função print para simplificar a utilização do System.out.println
         Função print para centralização de texto
